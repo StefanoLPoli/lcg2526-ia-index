ab1375d96cd1d7cad791a9e7213eff4349ce4293
Stefano Lorini <stefano.lorini@mail.polimi.it>
2025-10-14 11:44:09 +0200

    inclass dev

--- Class2/sketch.js
+++ Class2/sketch.js
@@ -4,13 +4,28 @@ let yMax = 600;
 let xRocket = xMax/2;
 let yRocket = yMax * 0.6;
 
+let star_img;
+let table;
+let rocket_img;
+
+//scalatura e rotazione
+let scalaDiBase = 1;
+let tempo = 1;
+let ruota2 = 1;
+
+function preload(){
+  table=loadTable("stars.csv","csv","header");
+  star_img = loadImage("star.png");
+  rocket_img = loadImage("rocket.png");
+}
+
 function setup() {
   createCanvas(xMax, yMax);
-  frameRate();
+  frameRate(10);
 }
 
 function draw() {
-  background("#090957ff");
+  background("#9595caff");
   //mostrare un testo bianco che mi dice le coordinate del mouse
   //sul foglio da disegno
   fill(255); //bianco
@@ -18,26 +33,69 @@ function draw() {
   text("MouseX: " + mouseX + ", MouseY: " + mouseY ,20,20);
 
   //disegno delle stelle in modo 1
-  //stelle1();
+  //stelle1(80);
 
   //disegno delle stelle con il random
-  stelleRandom();
+  //stelleRandom(80);
 
+  //disegno delle stelle dal file
+  disegnaStelleDaFile();
+
+  //disegno il razzo
+  let variazioneScala = scalaDiBase * Math.abs(sin(tempo));
+  disegnaRazzo(xRocket, yRocket,1,ruota2);
+  //disegnaRazzoDaFile(xRocket,yRocket);
+  tempo += 1;
+  ruota2 += 1;
+  yRocket = muoviRazzo(1, yRocket);
+}
+
+function stelleRandom(numeroStelle=120) //se non viene passato nessun parametro, disegnerà 120 stelle
+{
+  //disegnare le stelle
+  //120 stelle di 3 tipi diversi
+  //le stelle sono elissoidi
+  push();
+  for(let i=0; i<numeroStelle; i++)
+  {
+    let starX= random(0,xMax);
+    let starY= random(0,yMax);
+    let Trasparency = random(150,255);
+    let Size = random(5,10);
+    noStroke();
+
+    //stella di tipo A
+    fill(255,255,Trasparency);
+    ellipse(starX,starY,Size);
+  }
+  pop();
+}
+
+function disegnaRazzo(xRocket, yRocket, scalaB=1, ruota=30)
+{
   //inizio contesto di disegno
   push();
 
+  //scalare
+  scale(scalaB);
+
+  //ruotare
+  //prendere il pivot dal centro del foglio al centro del razzo
+  translate(xRocket/2, yRocket/2);
+  rotate(ruota);
+
   //rettangolo
   fill(200);
+  rectMode(CENTER);
   strokeWeight(1);
   stroke(40);
-  rectMode(CENTER);
-  rect(xRocket,yRocket+30,80,180,20);
+  rect(0,0+30,80,180,20);
 
   //cerchio
   fill(100, 149, 237);
   strokeWeight(3);
   stroke(255,255,255);
-  circle(xRocket,yRocket+10,50);
+  circle(0,0+10,50);
 
   //ellipse(300,200,100,50);
 
@@ -45,76 +103,84 @@ function draw() {
   fill(220, 20, 60);
   strokeWeight(1); 
   stroke(0,0,0);
-  triangle(xRocket-40,yRocket-60,xRocket+40,
-            yRocket-60,xRocket,yRocket-120);
+  triangle(0-40,0-60,0+40,
+            0-60,0,0-120);
 
-  triangle(xRocket+25,yRocket+80,xRocket+40,
-            yRocket+80,xRocket+80,yRocket+150);
+  triangle(0+25,0+80,0+40,
+            0+80,0+80,0+150);
 
-  triangle(xRocket-25,yRocket+80,xRocket-40,
-            yRocket+80,xRocket-80,yRocket+150);
-  
+  triangle(0-25,0+80,0-40,
+            0+80,0-80,0+150);
 
   //fine contesto di disegno
   pop();
+}
 
+function muoviRazzo(numeroPixel=1, yRocket)
+{
   //xRocket = (xRocket + 1) % xMax;
-  yRocket = yRocket - 1;
+  yRocket = yRocket - numeroPixel;
   //quando yRocket va oltre il canvas,
-  //allora va resettata
+  //allora la yRocket va resettata
   let soglia = - (yMax*0.6);
   if (yRocket < soglia){
     yRocket = yMax;
   }
+
+  return(yRocket); //ritorniamo il valore di yRocket
 }
 
-function stelle1()
+function stelle1(numeroStelle=120)
 {
   //disegnare le stelle
   //120 stelle di 3 tipi diversi
   //le stelle sono elissoidi
   push();
-  for(let i=0; i<120; i++)
+  for(let i=0; i<numeroStelle; i++)
   {
     let starX= (i*37) % width + (i%3) *5;
     let starY= (i*73) % height + (i%7);
+    noStroke();
+
+    let Trasparency = random(150,255);
+    let Size = random(5,10);
     noStroke();
     
     if((i%3)==0)
     {
       //stella di tipo A
-      fill(255,255,150);
-      ellipse(starX,starY,1);
+      fill(255,255,Trasparency);
+      ellipse(starX,starY,Size);
     } else if((i%3)==1){
       //stella di tipo B
-      fill(200,100,255);
-      ellipse(starX,starY,1.5);
+      fill(200,100,Trasparency);
+      ellipse(starX,starY,Size);
     } else {
       //stella di tipo C
-      fill(255,255,100);
-      ellipse(starX,starY,2.8);
+      fill(255,255,Trasparency);
+      ellipse(starX,starY,Size);
     }
   }
   pop();
 }
 
-function stelleRandom()
+function disegnaStelleDaFile()
 {
-  //disegnare le stelle
-  //120 stelle di 3 tipi diversi
-  //le stelle sono elissoidi
-  push();
-  for(let i=0; i<120; i++)
+  for(let k=0; k<table.getRowCount(); k++)
   {
-    let starX= random(0,xMax);
-    let starY= random(0,yMax);
-    let starTrasparency = random(150,255);
-    let starSize = random(1, 2.8);
-    noStroke();
-
-    //stella di tipo A
-    fill(255,255,starTrasparency);
-    ellipse(starX,starY,starSize);
+    let starX= (k*37) % width + (k%3) *5;
+    let starY= (k*73) % height + (k%7);
+    disegnaSignolaStellaDaFile(k, starX, starY);
   }
-  pop();
+}
+
+function disegnaSignolaStellaDaFile(index, posX, posY)
+{
+  let starSize = table.getNum(index, "starSize");
+  image(star_img, posX, posY, starSize, starSize);
+}
+
+function disegnaRazzoDaFile(rocketX, rocketY)
+{
+  image(rocket_img,rocketX-50,rocketY,100,100);
 }
